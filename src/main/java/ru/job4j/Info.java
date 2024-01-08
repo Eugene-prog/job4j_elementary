1de7884ec9aade029160f5fecff7eefe624cadbb
Eugene-prog <ieugene.prog@gmail.com>
2024-01-08 20:54:54 +0700

    Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/Info.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Info {
+    public static void main(String[] args) {
+        java.time.LocalDate currentDate = java.time.LocalDate.now();
+        System.out.println(currentDate);
+    }
+}

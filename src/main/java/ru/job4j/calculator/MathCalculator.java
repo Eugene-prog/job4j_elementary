e6fb4d1d05da0771bbc0f941fefc1d6bdcaaa9db
Eugene-prog <ieugene.prog@gmail.com>
2024-01-10 21:31:50 +0700

    Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,6 +1,7 @@
 package ru.job4j.calculator;
 
 import static ru.job4j.calculator.MathFunction.*;
+
 public class MathCalculator {
     public static double sumAndMultiply(double first, double second) {
         return sum(first, second) + multiply(first, second);

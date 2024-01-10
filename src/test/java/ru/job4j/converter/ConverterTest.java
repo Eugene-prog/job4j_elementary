788e41b1b59b07356eac09240eb4d6fbcf0755ee
Eugene-prog <ieugene.prog@gmail.com>
2024-01-10 11:08:01 +0700

    Модульные тесты [#159435]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -2,9 +2,7 @@ package ru.job4j.converter;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.*;
-
 
 class ConverterTest {
     @Test

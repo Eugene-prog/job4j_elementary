b243199b852006bae1ba868c0db1e6e48649cdc5
Eugene-prog <ieugene.prog@gmail.com>
2024-01-12 11:58:20 +0700

    Простые числа [#156314]

---
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PrimeNumberTest {
+    @Test
+    void when5Then3() {
+        int finish = 5;
+        int result = PrimeNumber.calc(finish);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when11Then5() {
+        int finish = 11;
+        int result = PrimeNumber.calc(finish);
+        int expected = 5;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when2Then1() {
+        int finish = 2;
+        int result = PrimeNumber.calc(finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when25Then9() {
+        int finish = 25;
+        int result = PrimeNumber.calc(finish);
+        int expected = 9;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void when49Then15() {
+        int finish = 49;
+        int result = PrimeNumber.calc(finish);
+        int expected = 15;
+        assertThat(result).isEqualTo(expected);
+    }
+}

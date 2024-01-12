e05192b3403b06e9ac272c7ecd246bc0097a7fee
Eugene-prog <ieugene.prog@gmail.com>
2024-01-12 13:24:57 +0700

    Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount > 0) {
+              amount = (amount + amount * percent * 0.01) - salary;
+              year++;
+        }
+        return year;
+    }
+}

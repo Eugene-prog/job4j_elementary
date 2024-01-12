1c34b894a43cfc49a558da4fe2c351cbbbc201ba
Eugene-prog <ieugene.prog@gmail.com>
2024-01-12 15:22:40 +0700

    Заполнение массива [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,10 +5,20 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+
+        names[0] = "Artem Smirnov";
+        names[1] = "Ekaterina Ivanova";
+        names[2] = "Dmitry Petrov";
+        names[3] = "Anna Kozlova";
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 
 }

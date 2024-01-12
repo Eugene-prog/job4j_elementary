9f95caca60be45a6f553560330e5520f4d2ddb99
Eugene-prog <ieugene.prog@gmail.com>
2024-01-12 15:15:24 +0700

    Объявление массива [#3613]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,10 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println(ages.length);
+        System.out.println(surnames.length);
+        System.out.println(prices.length);
     }
+
 }

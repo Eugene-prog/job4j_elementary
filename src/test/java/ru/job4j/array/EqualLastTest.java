8e3a548df1f6bae537db21684006a42656b58178
Eugene-prog <ieugene.prog@gmail.com>
2024-01-13 16:10:48 +0700

    Сравнить последние элементы двух массивов [#304228]

---
+++ src/test/java/ru/job4j/array/EqualLastTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EqualLastTest {
+    @Test
+    public void whenEqual() {
+        int[] left = {1, 2, 3};
+        int[] right = {5, 4, 3};
+        boolean result = EqualLast.check(left, right);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotEqual() {
+        int[] left = {1, 2, 3};
+        int[] right = {3, 3, 4};
+        boolean result = EqualLast.check(left, right);
+        assertThat(result).isFalse();
+    }
+}

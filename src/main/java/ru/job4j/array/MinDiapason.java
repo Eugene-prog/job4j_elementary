28350bf9acaccf95e30fe5e0a605f731737516bf
Eugene-prog <ieugene.prog@gmail.com>
2024-01-13 17:52:03 +0700

    Поиск минимума в диапазоне [#179804]

---
+++ src/main/java/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int index = start; index <= finish; index++) {
+            if (min > array[index]) {
+                min = array[index];
+            }
+        }
+        return min;
+    }
+}

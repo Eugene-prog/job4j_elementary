0681ee6746596fad7f7177eeae542f29be501059
Eugene-prog <ieugene.prog@gmail.com>
2024-01-13 11:44:59 +0700

    Создать программу, вычисляющую факториал [#193 #472631]

--- src/main/java/ru/job4j/loop/Factorial.java
+++ src/main/java/ru/job4j/loop/Factorial.java
@@ -3,16 +3,16 @@ package ru.job4j.loop;
 public class Factorial {
     public static int calculate(int number) {
         if (number < 0) {
+            System.out.println("Факториал не определен для отрицательных чисел.");
             return 0;
         }
-        int result = 1;
-        if ((number == 0) || (number == 1)) {
-            return result;
+        if (number == 0 || number == 1) {
+            return 1;
         }
-        for (int i = 1; i <= number; i++) {
+        int result = 1;
+        for (int i = 2; i <= number; i++) {
             result *= i;
         }
         return result;
     }
 }
-

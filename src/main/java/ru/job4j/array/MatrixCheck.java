b74a4e90edbe8532bb0fa123f840f1c33cc35159
Eugene-prog <ieugene.prog@gmail.com>
2024-01-14 14:50:03 +0700

    Моно строка в матрице [#214126]

---
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+            for (int cell = 0; cell < board[row].length; cell++) {
+                if (board[row][cell] != 'X') {
+                    result = false;
+                    break;
+                }
+            }
+        return result;
+    }
+}
+

b9c60277421380958ae957829b77819a774feefc
Eugene-prog <ieugene.prog@gmail.com>
2024-01-14 15:37:55 +0700

    Массив из диагонали матрицы [#214128]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -22,5 +22,13 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static char[] extractDiagonal(char[][] board) {
+        char[] result = new char[board.length];
+        for (int index = 0; index < board.length; index++) {
+            result[index] = board[index][index];
+        }
+        return result;
+    }
 }
 

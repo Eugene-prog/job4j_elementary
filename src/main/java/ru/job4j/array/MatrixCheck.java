8e7469c72018bef35d4e9bda90e90c9f15410c7e
Eugene-prog <ieugene.prog@gmail.com>
2024-01-15 10:11:37 +0700

    fix Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-            for (int cell = 0; cell < board[row].length; cell++) {
-                if (board[row][cell] != 'X') {
-                    result = false;
-                    break;
-                }
+        for (int cell = 0; cell < board[row].length; cell++) {
+            if (board[row][cell] != 'X') {
+                result = false;
+                break;
             }
+        }
         return result;
     }
 
@@ -35,11 +35,9 @@ public class MatrixCheck {
         boolean result = false;
         char[] diagonal = extractDiagonal(board);
         for (int index = 0; index < diagonal.length; index++) {
-            if (diagonal[index] == 'X') {
-                if (monoHorizontal(board, index) || monoVertical(board, index)) {
-                    result = true;
-                    break;
-                }
+            if (diagonal[index] == 'X' && (monoHorizontal(board, index) || monoVertical(board, index))) {
+                result = true;
+                break;
             }
         }
         return result;

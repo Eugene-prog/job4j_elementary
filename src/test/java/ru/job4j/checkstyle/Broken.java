b8eede996dd1036cd8a2fe89386701ac5b7c5fae
Eugene-prog <ieugene.prog@gmail.com>
2024-01-15 22:05:30 +0700

    Правила Checkstyle [#504981]

---
+++ src/test/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,25 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    private String name;
+    public String surname;
+    public static final String NEW_VALUE = "";
+
+    Broken() {
+    }
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g, int h) {
+
+    }
+}

d7bd068a0936c869cdf3eb231f0e21bcf8cb4b87
Eugene-prog <ieugene.prog@gmail.com>
2024-01-15 22:10:12 +0700

    fix Правила Checkstyle [#504981]

--- src/test/java/ru/job4j/checkstyle/Broken.java
+++ src/test/java/ru/job4j/checkstyle/Broken.java
@@ -19,7 +19,7 @@ public class Broken {
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g, int h) {
+    void method(int a, int b, int c, int d, int e, int f, int g) {
 
     }
 }

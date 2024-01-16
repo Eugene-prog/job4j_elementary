ce31644648a64b6681d8ed4ab76c0946bfa31828
Eugene-prog <ieugene.prog@gmail.com>
2024-01-16 10:37:02 +0700

    Создание объекта [#165452]

---
+++ src/main/java/oop/Dog.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}

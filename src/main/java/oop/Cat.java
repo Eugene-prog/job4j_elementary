3c489e9c014ed8f9735298bfb1f2b4a90e8a43ec
Eugene-prog <ieugene.prog@gmail.com>
2024-01-16 11:34:15 +0700

    Поля объекта [#166083]

---
+++ src/main/java/oop/Cat.java
@@ -0,0 +1,32 @@
+package oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(this.name + " eat " + this.food);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}

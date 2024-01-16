dba9a5cde1dda32d614534ee6fc1ea8330eba8c2
Eugene-prog <ieugene.prog@gmail.com>
2024-01-16 10:44:50 +0700

    Вызов метода объекта [#166085]

---
+++ src/main/java/oop/Student.java
@@ -0,0 +1,21 @@
+package oop;
+
+public class Student {
+    public void music() {
+        System.out.println("Tra tra tra");
+    }
+
+    public void sing() {
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.sing();
+        petya.sing();
+        petya.sing();
+    }
+}

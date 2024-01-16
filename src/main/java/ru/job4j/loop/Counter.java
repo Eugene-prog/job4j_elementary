c9ae10240a553f9861c7472cb3557c292db52cc1
Eugene-prog <ieugene.prog@gmail.com>
2024-01-16 17:41:47 +0700

    fix Сумма чисел [#226416 #472576]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,9 +3,6 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        if (start > finish) {
-            return sum;
-        }
         for (int i = start; i <= finish; i++) {
             sum += i;
         }

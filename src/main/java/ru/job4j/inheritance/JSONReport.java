836b20ece0bb1df4dfd00b6f7ea630919b6fc597
Eugene-prog <ieugene.prog@gmail.com>
2024-01-18 17:04:06 +0700

    Переопределение метода [#174890]

---
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -0,0 +1,11 @@
+package ru.job4j.inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        String ln = System.lineSeparator();
+        return "{" + ln
+                + "\t\"name\" : \"" + name + "\"," + ln
+                + "\t\"body\" : \"" + body + "\"" + ln
+                + "}";
+    }
+}

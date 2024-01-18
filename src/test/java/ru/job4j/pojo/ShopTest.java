5a593c44f089a144c0bd4528149b47dd5997be85
Eugene-prog <ieugene.prog@gmail.com>
2024-01-18 20:58:12 +0700

    Массив с пустыми ячейками [#176470]

---
+++ src/test/java/ru/job4j/pojo/ShopTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.pojo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ShopTest {
+    @Test
+    public void whenLastNull() {
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        int result = Shop.indexOfNull(products);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenFirstNull() {
+        Product[] products = new Product[5];
+        products[1] = new Product("Milk", 10);
+        int result = Shop.indexOfNull(products);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenHasNotNull() {
+        Product[] products = new Product[1];
+        products[0] = new Product("Milk", 10);
+        int result = Shop.indexOfNull(products);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+}

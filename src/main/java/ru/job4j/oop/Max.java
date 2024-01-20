e7395c30b8acd27f51c98d33c78ff0511324727c
Eugene-prog <ieugene.prog@gmail.com>
2024-01-20 16:06:55 +0700

    fix Перегрузить метод max для трех чисел [#122644 #474259]

--- src/main/java/ru/job4j/oop/Max.java
+++ src/main/java/ru/job4j/oop/Max.java
@@ -6,10 +6,10 @@ public class Max {
     }
 
     public int max(int one, int two, int three) {
-        return max(one, two) > three ? max(one, two) : three;
+        return max(max(one, two), three);
     }
 
     public int max(int one, int two, int three, int four) {
-        return max(one, two) > max(three, four) ? max(one, two) : max(three, four);
+        return max(max(one, two), max(three, four));
     }
 }

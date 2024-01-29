fd19bf2454e53490a54c79f5400bf86fb7180445
Eugene-prog <ieugene.prog@gmail.com>
2024-01-29 20:51:52 +0700

    fix Тестирование исключений с junit [#219366 #477156]

--- src/test/java/ru/job4j/ex/FactorialTest.java
+++ src/test/java/ru/job4j/ex/FactorialTest.java
@@ -2,11 +2,19 @@ package ru.job4j.ex;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.withPrecision;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FactorialTest {
+    @Test
+    public void whenFactorial12Then479001600() {
+        short input = 12;
+        double expected = 479001600.00;
+        double output = Factorial.calc(input);
+        assertThat(output).isEqualTo(expected, withPrecision(0.01));
+    }
+
     @Test
     public void whenExceptionFactorialTest() {
         IllegalArgumentException exception = assertThrows(

48895941397cf800e69c5a9bcfa507bc1d502114
Eugene-prog <ieugene.prog@gmail.com>
2024-02-02 00:06:52 +0700

    Принципы раннего возврата и охранных выражений [#504875]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,70 @@
+package ru.job4j.early;
+
+public class PasswordValidator {
+    private static final String[] FORBIDDEN = {"qwerty", "12345", "password", "admin", "user"};
+
+    public static boolean[] validatePasswordInfo(String password) {
+        boolean hasUpCase = false;
+        boolean hasLowCase = false;
+        boolean hasDigit = false;
+        boolean hasSpecial = false;
+        for (char symbol : password.toCharArray()) {
+            if (Character.isUpperCase(symbol)) {
+                hasUpCase = true;
+            }
+            if (Character.isLowerCase(symbol)) {
+                hasLowCase = true;
+            }
+            if (Character.isLowerCase(symbol)) {
+                hasLowCase = true;
+            }
+            if (Character.isDigit(symbol)) {
+                hasDigit = true;
+            }
+            if (!Character.isLetterOrDigit(symbol)) {
+                hasSpecial = true;
+            }
+        }
+        return new boolean[]{hasUpCase, hasLowCase, hasDigit, hasSpecial};
+    }
+
+    public static String validate(String password) {
+        if (password == null || password.isEmpty()) {
+            throw new IllegalArgumentException("Password can't be null");
+        }
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Password should be length [8, 32]");
+        }
+        for (String qwerty : FORBIDDEN) {
+            if (password.toLowerCase().contains(qwerty.toLowerCase())) {
+                throw new IllegalArgumentException(
+                        "Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            }
+        }
+        boolean hasUpCase = validatePasswordInfo(password)[0];
+        boolean hasLowCase = validatePasswordInfo(password)[1];
+        boolean hasDigit = validatePasswordInfo(password)[2];
+        boolean hasSpecial = validatePasswordInfo(password)[3];
+        if (!hasUpCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one uppercase letter"
+            );
+        }
+        if (!hasLowCase) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one lowercase letter"
+            );
+        }
+        if (!hasDigit) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one figure"
+            );
+        }
+        if (!hasSpecial) {
+            throw new IllegalArgumentException(
+                    "Password should contain at least one special symbol"
+            );
+        }
+        return password;
+    }
+}
